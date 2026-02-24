e523e90a863e2b40d5baf7b2ea8d6c3687c3c59f
balding230wall <chuyanwang595@gmail.com>
2026-02-24 10:48:33 -0600

    Added interactive scene

---
+++ Interactive-Scene/sketch.js
@@ -0,0 +1,48 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let turn;
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  drawBack();
+  playerOne();
+  playerTwo();
+  
+}
+
+function drawBack(){
+  rect(25, 10, 350, 390);
+  for (let step1 = 0.70; step1 < 8; step1++){
+    for (let step = 1; step < 8; step++){
+      circle(step * 50, step1 * 50, 25);
+    }
+  }
+}
+
+function mouseClicked(){
+  turn = !turn;
+
+}
+
+function playerOne(){
+  if (turn === true){
+    fill("red");
+  }
+}
+
+function playerTwo(){
+  if (turn === false){
+    fill("yellow");
+  }
+}
