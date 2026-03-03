3955e0fc291b21a1ad18180729e2a949672d6f46
balding230wall <chuyanwang595@gmail.com>
2026-03-02 21:49:10 -0600

    added win check and ability to place pieces. Tidied up code, removed placeholder/unnecessary values. Swapped array creation to a simpler method, added comments and created better variable names

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -3,81 +3,200 @@
 // March 3, 2026
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Used a 2D array to create the board for the game
+
+//setting variables
+let turn = true;
+let selectionColumn = 25;
+
+//creating a 2D array
+let board = [[0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0]];
 
 
-let turn;
-let column = 25;
-let board = [];
 
-
-
-function setup() {
+function setup(){
+  //create the canvas
   createCanvas(400, 400);
-
-  for (let r = 0; r < 7; r++) {
-    board[r] = [];
-    for (let c = 0; c < 7; c++) {
-      board [r] [c] = 0;
-    }
-  }
 }
 
-function draw() {
-  playerOne();
-  playerTwo();
+
+function draw(){
+  //calls the functions below
   drawBackground();
+  drawPieces();
   selectionBox();
 }
 
+
 function drawBackground(){
-  fill("black");
+  //draws a green rectangle that acts for the game board
+  fill(144, 238, 144);
   rect(25, 0, 350, 380);
 
-  fill("white")
-  for (let r = 0; r < 7; r++){
-    for (let c = 0; c < 7; c++){
-      circle(c * 50 + 50, r * 50 + 50, 40);
+  //using a nested loop to draw circles to create an empty board
+  for (let boardRow = 0; boardRow < 7; boardRow++){
+    for (let boardColumn = 0; boardColumn < 7; boardColumn++){
+      fill("white");
+      circle(boardColumn * 50 + 50, boardRow * 50 + 50, 40);
     }
   }
-}
+ }
 
-function mouseClicked(){
-  turn = !turn;
-
-}
-
-function playerOne(){
-  if (turn === true){
-    fill("red");
-  }
-}
-
-function playerTwo(){
-  if (turn === false){
-    fill("yellow");
-  }
-}
 
 function selectionBox(){
+  //creates a translucent box that highlights the currently selected column
   fill(0, 100);
-  rect(column, 0, 50, 370);
+  rect(selectionColumn, 0, 50, 380);
 }
 
+
 function keyPressed(){
+  //moves the selection box to the right when right arrow is pressed
   if (keyCode === RIGHT_ARROW){
     fill(0, 100);
-    rect(column, 0, 50, 380);
-    if (column < 300){
-      column = column + 50;
+    rect(selectionColumn, 0, 50, 380);
+    if (selectionColumn < 300){
+      selectionColumn = selectionColumn + 50;
     }
   }
   
+  //moves the selection box to the left when left arrow is pressed
   if (keyCode === LEFT_ARROW){
     fill(0, 100);
-    rect(column, 0, 50, 380);
-    if (column > 50){
-      column = column - 50;
+    rect(selectionColumn, 0, 50, 380);
+    if (selectionColumn > 50){
+      selectionColumn = selectionColumn - 50;
     }
   } 
 }
+
+
+function mouseClicked(){
+  //converts selection box position to a position in the array
+  let currentColumn = (selectionColumn - 25) / 50;
+  
+  //calls the place piece function
+  placePiece(currentColumn);
+
+  //checks whether someone has won
+  checkWin();
+  
+  //swaps turns between the players
+  turn = !turn;
+}
+
+
+function placePiece(currentColumn) {
+  /*
+  checks every row in the selected column to see if it has already been claimed by a player, 
+  starting from the bottom. When it finds an unclaimed row, indicate whether it will now be claimed
+  by player one or two, depending on the turn.
+  */
+  for (let pieceRow = 6; pieceRow >= 0; pieceRow--){
+    if (board[pieceRow][currentColumn] === 0) {
+      if (turn === true){
+        board[pieceRow][currentColumn] = 1;
+      }
+      else{
+        board[pieceRow][currentColumn] = 2;
+      }
+      break;
+    }
+  }
+}
+
+
+function drawPieces() {
+/*
+constantly checks each value in the array. If it finds a value claimed by a player (either turning to
+one or two), created a colored circle there based on who claimed it.
+*/
+    for (let pieceRow = 0; pieceRow < 7; pieceRow++){
+      for (let pieceColumn = 0; pieceColumn < 7; pieceColumn++){
+        
+        if (board[pieceRow][pieceColumn] === 1){
+          fill("red");
+          circle(pieceColumn * 50 + 50, pieceRow * 50 + 50, 40);
+        }
+       
+        if (board[pieceRow][pieceColumn] === 2){
+          fill("yellow");
+          circle(pieceColumn * 50 + 50, pieceRow * 50 + 50, 40);
+        }
+    }
+  }
+}
+
+function checkWin(){
+  //checks for a possible horizontal win
+  for (let boardRow = 0; boardRow < 7; boardRow++){
+    for (let boardColumn = 0; boardColumn < 4; boardColumn++){
+      let checkForFour = board[boardRow][boardColumn];
+      if (checkForFour !== 0 && checkForFour === board[boardRow][boardColumn + 1] &&
+          checkForFour === board[boardRow][boardColumn + 2] && checkForFour === board[boardRow][boardColumn + 3]){
+        if (checkForFour === 1){
+          console.log("Red Wins!");
+        }
+        else if (checkForFour === 2){
+          console.log("Yellow Wins!");
+        }
+      }
+    }
+  }
+
+  //checks for a possible vertical win
+  for (let boardColumn = 0; boardColumn < 7; boardColumn++){
+    for (let boardRow = 0; boardRow < 4; boardRow++){
+      let checkForFour = board[boardRow][boardColumn];
+      if (checkForFour !== 0 && checkForFour === board[boardRow + 1][boardColumn] &&
+          checkForFour === board[boardRow + 2][boardColumn] && checkForFour === board[boardRow + 3][boardColumn]){
+        if (checkForFour === 1){
+          console.log("Red Wins!");
+        }
+        else if (checkForFour === 2){
+          console.log("Yellow Wins!");
+        }
+      }
+    }
+  }
+
+  //checks for a possible diagonol win one way
+  for (let boardColumn = 0; boardColumn < 4; boardColumn++){
+    for (let boardRow = 0; boardRow < 4; boardRow++){
+      let checkForFour = board[boardRow][boardColumn];
+      if (checkForFour !== 0 && checkForFour === board[boardRow + 1][boardColumn + 1] &&
+          checkForFour === board[boardRow + 2][boardColumn + 2] && checkForFour === board[boardRow + 3][boardColumn + 3]){
+        if (checkForFour === 1){
+          console.log("Red Wins!");
+        }
+        else if (checkForFour === 2){
+          console.log("Yellow Wins!");
+        }
+      }
+    }
+  }
+
+  //checks for a possible diagonal win the other way
+  for (let boardColumn = 3; boardColumn < 7; boardColumn++){
+    for (let boardRow = 0; boardRow < 4; boardRow++){
+      let checkForFour = board[boardRow][boardColumn];
+      if (checkForFour !== 0 && checkForFour === board[boardRow + 1][boardColumn - 1] &&
+          checkForFour === board[boardRow + 2][boardColumn - 2] && checkForFour === board[boardRow + 3][boardColumn - 3]){
+        if (checkForFour === 1){
+          console.log("Red Wins!");
+        }
+        else if (checkForFour === 2){
+          console.log("Yellow Wins!");
+        }
+      }
+    }
+  }
+}
+
+
