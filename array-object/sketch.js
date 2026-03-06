0eb602b083efeaf7607d213beceba271e15e9040
balding230wall <chuyanwang595@gmail.com>
2026-03-06 11:02:07 -0600

    started coding for array project. Drew a circle and started adding colors

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,11 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+ctx.fillStyle = "#4efd54";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  let r = random(0, 255);
+  let g = random(0, 255);
+  let b = random(0, 255);
+  
+  for ( let i = windowHeight; i > 0; i--) {
+    fill();
+    noStroke();
+    ctx.fill();
+    circle(windowWidth/2, windowHeight/2, i);
+  }
 }
