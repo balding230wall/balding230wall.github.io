42614d06e5b71d7c6f051f8448a63e24267f1e8a
balding230wall <chuyanwang595@gmail.com>
2026-03-09 11:02:24 -0600

    removed previous values that I don't plan on using anymore. Started on creating a shrinking circle

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,21 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-ctx.fillStyle = "#4efd54";
+let circle = {
+  circleDiameter: windowHeight,
+  radiusDecay: 2
+};
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  let r = random(0, 255);
-  let g = random(0, 255);
-  let b = random(0, 255);
-  
-  for ( let i = windowHeight; i > 0; i--) {
-    fill();
-    noStroke();
-    ctx.fill();
-    circle(windowWidth/2, windowHeight/2, i);
-  }
+  fill("black");
+  noStroke();
+  circle(windowWidth/2, windowHeight/2, circle.circleDiameter);
+  circle.circleDiameter = circle.circleDiameter - circle.radiusDecay;
 }
