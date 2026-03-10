0ef461e063173d00c1e64071207cb8faff10777c
balding230wall <chuyanwang595@gmail.com>
2026-03-10 11:02:44 -0600

    made a shrinking ring. Prevented circle from expanding after its diameter reached 0

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -1,24 +1,47 @@
-// Project Title
-// Your Name
+// Cool Spiral Animation
+// Chuyan Wang
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let circle = {
-  circleDiameter: windowHeight,
-  radiusDecay: 2
+let circleOne = {
+  circleDiameterOne: 0,
+  radiusDecayOne: 10
 };
 
+let circleTwo = {
+  circleDiameterTwo: 0,
+  radiusDecayTwo: 10
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  circleOne.circleDiameterOne = windowHeight + 100;
+  circleTwo.circleDiameterTwo = circleOne.circleDiameterOne - 25;
 
 }
 
 function draw() {
-  fill("black");
+  background(0);
+  fill("#BC13FE");
   noStroke();
-  circle(windowWidth/2, windowHeight/2, circle.circleDiameter);
-  circle.circleDiameter = circle.circleDiameter - circle.radiusDecay;
+  circle(windowWidth/2, windowHeight/2, circleOne.circleDiameterOne);
+  
+  fill("black");
+  circle(windowWidth/2, windowHeight/2, circleTwo.circleDiameterTwo);
+
+  if (circleOne.circleDiameterOne >= 0){
+    circleOne.circleDiameterOne = circleOne.circleDiameterOne - circleOne.radiusDecayOne;
+  }
+  else{
+    circleOne.circleDiameterOne = windowHeight + 100;
+  }
+
+  if (circleTwo.circleDiameterTwo >= 0){
+    circleTwo.circleDiameterTwo = circleTwo.circleDiameterTwo - circleTwo.radiusDecayTwo;
+  }
+  else{
+    circleTwo.circleDiameterTwo = circleOne.circleDiameterOne - 25;
+  }
 }
